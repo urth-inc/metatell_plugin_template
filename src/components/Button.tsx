f94e83797332a15279528f3280cc815b678af670
kira924age <kira924age@gmail.com>
2024-07-25 18:31:31 +0900

    feat: 🎸 add MiniCSSPlugin

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import * as styles from "./Button.module.scss";
 
 const Button: React.FC = () => {
   const onClick = () => {
     console.log("crab button was clicked");
-  }
+  };
 
-  return <button onClick={onClick}>🦀Remote Button🦀</button>;
+  return (
+    <div className={styles.buttonContainer}>
+      <button onClick={onClick}>🦀Remote Button🦀</button>
+    </div>
+  );
 };
 
 export default Button;
