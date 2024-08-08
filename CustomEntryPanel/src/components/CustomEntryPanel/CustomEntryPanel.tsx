cb28d81d552f86400120f3e7b384798b18bac61c
Limeng Ruan <ruan.limeng@trans-cosmos.co.jp>
2024-08-08 15:46:32 +0900

    CustomEntryPanel Sample page implemented

--- CustomEntryPanel/src/components/CustomEntryPanel/CustomEntryPanel.tsx
+++ CustomEntryPanel/src/components/CustomEntryPanel/CustomEntryPanel.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { useCssBreakpoints } from "react-use-css-breakpoints"
 import * as styles from "./CustomEntryPanel.module.scss";
 
 interface CustomEntryPanelProps {
@@ -40,11 +41,147 @@ export const CustomEntryPanel: React.FC<CustomEntryPanelProps> = ({
   termsOfServiceUrl,
   privacyPolicyUrl,
 }) => {
+  const breakpoint = useCssBreakpoints()
+
+  const [agreeToTerms, setAgreeToTerms] = useState(false)
+  const [agreeToPrivacy, setAgreeToPrivacy] = useState(false)
+
+  const handleAgreeToTerms = () => {
+    console.log('aaaa')
+    setAgreeToTerms(!agreeToTerms)
+  }
+  const handleAgreeToPrivacy = () => setAgreeToPrivacy(!agreeToPrivacy)
+
+  const isButtonDisabled = () => {
+    if (termsOfServiceUrl && !agreeToTerms) return true
+    if (privacyPolicyUrl && !agreeToPrivacy) return true
+    return false
+  }
+
   return (
-    <div>
+    <div className={styles.tempWrapper}>
       {/* TODO: Implement your custom entry panel here */}
-      <h1>Custom Entry Panel</h1>
-      <img src={logoUrl} alt="logo" />
+      <div className={styles.modal}>
+        <div className={styles.modalContent}>
+          <div className={`${styles.container} ${styles.keyboard}`}>
+            <div>
+              <p>
+                {leftMessage ? (
+                  leftMessage
+                ) : (
+                  <div>デフォルトメッセージ</div>
+                )}
+              </p>
+            </div>
+            <div>{leftImage}</div>
+          </div>
+
+          <div className={`${styles.container} ${styles.mouse}`}>
+            <div>
+              <div>{rightImage}</div>
+            </div>
+            <div>
+              <p>
+                {rightMessage ? (
+                  rightMessage
+                ) : (
+                  <div>デフォルトメッセージ</div>
+                )}
+              </p>
+            </div>
+          </div>
+
+          <div className={`${styles.column} ${styles.center} ${styles.centerContent}`}>
+            {breakpoint !== "sm" && breakpoint !== "md" && (
+              <div className={styles.logoContainer}>
+                <img src={logoUrl} alt="logo" />
+              </div>
+            )}
+
+            <div className={styles.roomName}>
+              <p>{roomName}</p>
+            </div>
+
+            <div className={`${styles.column} ${styles.center}`}>
+              {(termsOfServiceUrl || privacyPolicyUrl) && (
+                <div style={{ marginBottom: '16px' }}>
+                  {termsOfServiceUrl && (
+                    <div className={styles.termsCheckboxBlock}>
+                      <label className={styles.checkboxInput}>
+                        <input type="checkbox" checked={agreeToTerms} onChange={handleAgreeToTerms} />
+                        <div className={styles.checkmark} />
+                        <div className={styles.labelContainer}>
+                          <p>
+                            <a className={styles.customA} href={termsOfServiceUrl} target="_blank" rel="noopener noreferrer">利用規約</a>
+                            に同意する
+                          </p>
+                        </div>
+                      </label>
+                    </div>
+                  )}
+
+                  {privacyPolicyUrl && (
+                    <div className={styles.termsCheckboxBlock}>
+                      <label className={styles.checkboxInput}>
+                        <input type="checkbox" checked={agreeToPrivacy} onChange={handleAgreeToPrivacy} />
+                        <div className={styles.checkmark} />
+                        <div className={styles.labelContainer}>
+                          <p>
+                            <a className={styles.customA} href={termsOfServiceUrl} target="_blank" rel="noopener noreferrer">プライバシーポリシー</a>
+                            に同意する
+                          </p>
+                        </div>
+                      </label>
+                    </div>
+                  )}
+                </div>
+              )}
+
+              {showJoinRoom && (
+                <button className={`${styles.buttonCommon} ${styles.accent4}`} onClick={onJoinRoom} disabled={isButtonDisabled()}>
+                  <span>
+                    <div>ルームに入る</div>
+                  </span>
+                </button>
+              )}
+
+              {showEnterOnDevice && (
+                <button className={`${styles.buttonCommon} ${styles.accent5}`} onClick={onEnterOnDevice} disabled={isButtonDisabled()}>
+                  <span>
+                    <div>VRヘッドセットで入る</div>
+                  </span>
+                </button>
+              )}
+
+              {showSpectate && (
+                <button className={`${styles.buttonCommon} ${styles.accent2}`} onClick={onSpectate} disabled={isButtonDisabled()}>
+                  <span>
+                    <div>ツアーに参加する</div>
+                  </span>
+                </button>
+              )}
+
+              {isRoomFull && (
+                <div className={styles["full-room-description-container"]}>
+                  <p>只今満員のため、他の方が退室されるまでお待ちください。</p>
+                  <p>入室可能になりましたらボタンが表示されますので入室ください。</p>
+                </div>
+              )}
+
+              {showOptions && breakpoint !== "sm" && (
+                <>
+                  <hr className={styles.customHr} />
+                  <button className={`${styles.buttonCommon} ${styles.transparent}`} onClick={onOptions}>
+                    <span>
+                      <div>ルームの設定</div>
+                    </span>
+                  </button>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
