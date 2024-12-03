4075b6467426b0df87d4211fd3037f0dbe050ede
Limeng Ruan <ruan.limeng@trans-cosmos.co.jp>
2024-12-03 12:27:51 +0900

    update package-lock, use createRoot for react 18

--- AdditionalToolbarButton/src/bootstrap.tsx
+++ AdditionalToolbarButton/src/bootstrap.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { render } from "react-dom";
+import { Container, createRoot } from 'react-dom/client';
 
 import App from "./App";
 import "./styles/global.module.scss";
 
 const remoteElement = document.getElementById("root");
-render(
+const root = createRoot(remoteElement as Container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  remoteElement,
+  </React.StrictMode>
 );
--- CustomProfileModal/src/bootstrap.tsx
+++ CustomProfileModal/src/bootstrap.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { render } from "react-dom";
+import { Container, createRoot } from 'react-dom/client';
 
 import App from "./App";
 
 const remoteElement = document.getElementById("ui-root");
-render(
+const root = createRoot(remoteElement as Container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  remoteElement,
+  </React.StrictMode>
 );
